ba339acbf81096cf68df792300b617b605a3f3d9
Mary Rastorgueva <mari.rast4722@gmail.com>
2024-02-08 21:48:23 +0300

    #3 - Data validation (with Pydentic)

--- main.py
+++ main.py
@@ -1,36 +1,66 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app=FastAPI(
     title="Trading App"
 )
+
+
 fake_users = [
     {"id": 1, "role": "trader", "name": "John Doe"},
     {"id": 2, "role": "analyst", "name": "Jane Smith"},
-    {"id": 3, "role": "investor", "name": "David Johnson"}
+    {"id": 3, "role": "investor", "name": "David Johnson"},
+    {"id": 4, "role": "investor", "name": "Homer Simpson",
+     "degree": [{"id": 1, "created_at": "2022-01-15", "type_degree": "newbie"}]}
+
 ]
-@app.get("/users/{user_id}")
+
+
+class DegreeType(Enum):
+    newbie="newbie"
+    expert="expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]]=[]
+
+
+@app.get("/users/{user_id}",response_model=List[User])
 def hello(user_id: int):
     return [user for user in fake_users if user.get("id")==user_id]
 
-fake_trade = [
+
+fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 40000, "amount": 0.05},
     {"id": 2, "user_id": 2, "currency": "ETH", "side": "sell", "price": 3000, "amount": 1.5},
     {"id": 3, "user_id": 3, "currency": "LTC", "side": "buy", "price": 200, "amount": 5}
 ]
 
-@app.get("/trades")
-def get_trades(limit: int=1,offset: int=0):
-    return fake_trade[offset:][:limit]
 
-fake_users2 = [
-    {"id": 1, "role": "trader", "name": "John Doe"},
-    {"id": 2, "role": "analyst", "name": "Jane Smith"},
-    {"id": 3, "role": "investor", "name": "David Johnson"}
-]
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str=Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
 
-@app.post("/users/{user_id}")
-def chage_user_name(user_id:int,new_name:str):
-    current_user=list(filter(lambda user:user.get("id")==user_id,fake_users2))[0]
-    current_user["name"]=new_name
-    return {"status":200,"data":current_user}
+
+@app.post("/trades")
+def add_trades(trades:List[Trade]):
+    fake_trades.extend(trades)
+    return {"status":200,"data":fake_trades}
 
